1775aab42c7d79aa1a00553e502bbf6971cfbfc7
Laila Vinson <lvinson@isbx.com>
2017-08-09 18:50:20 -0700

    serve more files in production to allow loading different routes

--- server/src/boot/static.ts
+++ server/src/boot/static.ts
@@ -2,15 +2,16 @@ import express = require('express');
 import path = require('path');
 
 export = (server) => {
-  let outDir = __dirname + '/../../../client/dist';
+  const outDir = __dirname + '/../../../client/dist';
 
   server.use(express.static(outDir));
 
-  if (server.get('env') ===  'development') {
-    server.get('*', (req, res) => {
-      const index = path.join(outDir, 'index.html');
-      res.sendFile(index);
-    });
-  }
+  server.get('*', (req, res) => {
+    if (server.get('env') !== 'development' && req.path.match(/\.(html|css|png|jpg|ttf|js|ico)$/)) {
+      res.status(404).send('Not found');
+    }
+    const index = path.join(outDir, 'index.html');
+    res.sendFile(index);
+  });
 
 };
